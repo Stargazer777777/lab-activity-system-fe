2452f55f3123953f34e752833ace88655d1d9460
stargazer <1601778118@qq.com>
2023-09-18 14:43:03 +0800

    java http interceptor

--- src/$http/index.ts
+++ src/$http/index.ts
@@ -1,53 +1,74 @@
 import axios, { AxiosError } from 'axios';
 import type { AxiosRequestConfig } from 'axios';
-export const httpInstance = axios.create();
+import { ElMessage } from 'element-plus';
 
-export type BkResponse = {
-  data: unknown; // 出错时这一项将没有
-  code: number;
+export interface BkResponse {
+  data: object; // 出错时这一项将没有
   msg: string;
   success: boolean;
-};
+}
 
-httpInstance.defaults.baseURL = import.meta.env.HTTP_BASEURL;
+export interface BkErrorResponse extends BkResponse {
+  detail: string;
+}
 
 export interface HttpOption {
   noAlert?: true;
 }
 
-export const $http = async (
-  config: AxiosRequestConfig,
-  httpOption?: HttpOption
-) => {
-  try {
-    const axiosResponse = await httpInstance<BkResponse>(config);
-    const bkResponse = axiosResponse.data;
-
-    if (!bkResponse?.success) {
-      let errTitle = 'Error';
-      if (bkResponse.code === 401) {
-        errTitle = 'Unauthorized';
-      } else if (bkResponse.code === 403) {
-        errTitle = 'Forbidden';
-      } else if (bkResponse.code === 406) {
-        errTitle = '406Error';
-      } else if (bkResponse.code === 500) {
-        errTitle = 'ServerError';
-      } else {
-        errTitle = 'Unknown';
-      }
-      if (!httpOption?.noAlert) {
-        alert(`${errTitle}: ${bkResponse.msg || 'unknown'}`);
-      }
-      const err = new Error(bkResponse?.msg || 'Unknown');
-      err.name = errTitle;
-      throw err;
-    }
-    return bkResponse;
-  } catch (err) {
-    if (err instanceof AxiosError) {
-      alert('网络错误');
-    }
-    throw err;
+export class BkError extends Error {
+  constructor(bkErrorResponse: BkErrorResponse) {
+    super();
+    super.stack = 'BkError';
+    super.message = bkErrorResponse.msg;
+    super.cause = bkErrorResponse.detail;
   }
-};
+}
+
+export class JavaHttpTool {
+  private static httpInstance = axios.create();
+
+  static setBaseUrl(baseUrl: string) {
+    JavaHttpTool.httpInstance.defaults.baseURL = baseUrl;
+  }
+
+  private static errHandler(err: AxiosError, httpOption?: HttpOption) {
+    const statusCode = err.status;
+
+    let errMsg = '未知错误';
+
+    let bkError: BkError | undefined;
+
+    if (err.response?.data) {
+      const errResponse = err.response.data as BkErrorResponse;
+      bkError = new BkError(errResponse);
+      errMsg = errResponse.msg;
+    }
+
+    switch (statusCode) {
+      default:
+        break;
+    }
+
+    if (!httpOption?.noAlert) {
+      ElMessage.error(errMsg);
+    }
+    throw bkError;
+  }
+
+  static async send(
+    config: AxiosRequestConfig,
+    httpOption?: HttpOption
+  ): Promise<BkResponse> {
+    try {
+      const axiosResponse = await JavaHttpTool.httpInstance<BkResponse>(config);
+      return axiosResponse.data as BkResponse;
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        throw JavaHttpTool.errHandler(err, httpOption);
+      } else {
+        throw err;
+      }
+    }
+  }
+}
