ca4e91f0550355afbd6f9763ca5cd3e93509b1ac
stargazer <1601778118@qq.com>
2023-09-23 22:55:58 +0800

    add padding to page

--- src/components/adminFrame/adminMenu.vue
+++ src/components/adminFrame/adminMenu.vue
@@ -2,6 +2,9 @@
   <el-menu mode="horizontal" :default-active="defaultActive" router>
     <el-col class="logo" :span="1" :offset="0">LOGO</el-col>
     <el-menu-item index="/admin" key="/admin">主页</el-menu-item>
+    <el-menu-item index="/admin/activity" key="/admin/activity"
+      >活动管理</el-menu-item
+    >
   </el-menu>
 </template>
 
